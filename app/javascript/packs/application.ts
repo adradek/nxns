79fa88cd4bd8fe7dbc980a4fbd9e9186d23f5ea3
Alex Kochurov <alex.kochurov@gmail.com>
2020-03-31 01:56:33 +0300

    generalized approach

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -41,8 +41,31 @@ class DayToggle {
   // 
 }
 
+import GenericToggle from "src/generic_toggle"
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".js--day").forEach(element => {
-    new DayToggle(element as HTMLElement).initHandlers()
+  document.querySelectorAll(".day-body").forEach(day => {
+    new GenericToggle(
+      day,
+      ".js--day-button",
+      ".js--day-text",
+      (element, _, hidden) => {
+        const buttonText: HTMLElement = day.querySelector(".js--button-text")
+        buttonText.innerText = hidden ? "Show" : "Hide"
+      }
+    ).initHandlers()
+  })
+
+  document.querySelectorAll(".js--schedule-day").forEach(button => {
+    const buttonSelector = `#${button.id}`
+    const targetSelector = buttonSelector.replace("toggle", "body")
+    new GenericToggle(
+      document,
+      buttonSelector,
+      targetSelector,
+      (element, _, hidden) => {
+        element.classList.toggle("has-text-danger", hidden)
+      }
+    ).initHandlers()
   })
 })
